62e9193735b75914ea4e4179358bf2c7235972d7
Hicran Filiz <h.filiz781@gmail.com>
2021-02-20 18:04:13 +0300

    added

--- nokta.ucgen.c
+++ nokta.ucgen.c
@@ -1,22 +1,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+
 
     float Ax,Ay,Bx,By,Cx,Cy,Px,Py;
     float bx,by,cx,cy,x,y;
     float k;
     float WA,WB,WC;
 
-float k_degeri (float k);
-float deger1 (float WA);
-float deger2 (float WB);
-float deger3 (float WC);
-float ara1 (float bx);
-float ara2 (float by);
-float ara3 (float cx);
-float ara4 (float cy);
-float x_degeri (float x );
-float y_degeri (float y);
+float k_degeri (float k,float bx,float cy,float cx,float by);
+float deger1 (float WA,float by,float cy, float y, float cx, float bx, float k);
+float deger2 (float WB,float x, float cy, float y, float cx, float k);
+float deger3 (float WC,float y,float bx , float x, float by, float k);
+float ara1 (float bx,float Bx,float Ax);
+float ara2 (float by,float By,float Ay);
+float ara3 (float cx,float Cx,float Ax);
+float ara4 (float cy,float Cy,float Ay);
+float x_degeri (float x , float Px, float Ax);
+float y_degeri (float y,float Py,float Ay);
 
 
 int main()
@@ -50,16 +52,16 @@ int main()
     //WA= (x*(by-cy)+y*(cx-bx)+bx*cy-cx*by)/k;
     //WB=(x*cy-y*cx)/k;
     //WC=(y*bx-x*by)/k;
-    ara1(bx);
-    ara2(by);
-    ara3(cx);
-    ara4(cy);
-    x_degeri(x);
-    y_degeri(y);
-    k_degeri(k);
-    deger1(WA);
-    deger2(WB);
-    deger3(WC);
+    ara1(bx,Bx,Ax);
+    ara2(by,By,Ay);
+    ara3(cx,Cx,Ax);
+    ara4(cy,Cy,Ay);
+    x_degeri(x,Px,Ax);
+    y_degeri(y,Py,Ay);
+    k_degeri(k,bx,cy,cx,by);
+    deger1(WA,by,cy,y,cx,bx,k);
+    deger2(WB,x,cy,y,cx,k);
+    deger3(WC,y,bx,x,by,k);
 
 
     if(WA>0 && WA<1 && WB>0 && WB<1 && WC>0 && WC<1)
@@ -71,47 +73,57 @@ int main()
     return 0;
 	}
 
-
-    float k_degeri (float k){
-
-        return( k=bx*cy-cx*by);
+    float ara1 (float bx,float Bx,float Ax){
+        bx=(Bx-Ax);
+        return((Bx-Ax));
     }
 
-    float deger1 (float WA){
-
-        return((x*(by-cy)+y*(cx-bx)+bx*cy-cx*by)/k);
+    float ara2 (float by,float By,float Ay){
+        by=(By-Ay);
+        return((By-Ay));
     }
 
-    float deger2 (float WB){
-        return((x*cy-y*cx)/k);
+    float ara3 (float cx,float Cx,float Ax){
+        cx=(Cx-Ax);
+        return((Cx-Ax));
     }
 
-    float deger3 (float WC){
-        return((y*bx-x*by)/k);
+    float ara4 (float cy,float Cy,float Ay){
+        cy=(Cy-Ay);
+        return((Cy-Ay));
     }
 
-    float ara1 (float bx){
-        return(Bx-Ax);
+    float x_degeri (float x , float Px, float Ax){
+        x=(Px-Ax);
+        return((Px-Ax));
     }
 
-    float ara2 (float by){
-        return(By-Ay);
+    float y_degeri (float y,float Py,float Ay){
+        y=(Py-Ay);
+        return((Py-Ay));
     }
 
-    float ara3 (float cx){
-        return(Cx-Ax);
+
+
+    float k_degeri (float k,float bx,float cy,float cx,float by){
+        k=((bx*cy)-(cx*by));
+        return((bx*cy)-(cx*by));
     }
 
-    float ara4 (float cy){
-        return(Cy-Ay);
+    float deger1 (float WA,float by,float cy, float y, float cx, float bx, float k){
+        WA=((x*(by-cy))+(y*(cx-bx))+((bx*cy)-(cx*by)))/k;
+        return(((x*(by-cy))+(y*(cx-bx))+((bx*cy))-((cx*by)))/k);
     }
 
-    float x_degeri (float x ){
-        return(Px-Ax);
+    float deger2 (float WB,float x, float cy, float y, float cx, float k){
+        WB=(((x*cy)-(y*cx))/k);
+        return(((x*cy)-(y*cx))/k);
     }
 
-    float y_degeri (float y){
-        return(Py-Ay);
+    float deger3 (float WC,float y,float bx , float x, float by, float k){
+        WC=(((y*bx)-(x*by))/k);
+        return(((y*bx)-(x*by))/k);
     }
 
+    
 
